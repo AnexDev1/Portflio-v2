6909af2f55418b5a67f233a5d19d67dcf3750c77
anexdev1 <anwarnasir0970@gmail.com>
2024-02-17 11:46:06 +0300

    added projects page

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,15 @@
 import React from "react";
 import { LampDemo } from "@/components/ui/lamp-header";
 import { FloatingNavDemo } from "@/components/ui/navbar";
+import { content } from "@/components/ui/projects";
+import { StickyScroll } from "@/components/ui/sticky-scroll-reveal";
 
 export default function Home() {
   return (
     <>
       <FloatingNavDemo />
       <LampDemo />
-
-      <div style={{ height: "100vh" }}>hell</div>
+      <StickyScroll content={content} />
     </>
   );
 }
